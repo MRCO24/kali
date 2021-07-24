226e0211b11dae5b7121e1524d3476e9e509f15f
mrco24 <77525344+MRCO24@users.noreply.github.com>
2021-07-24 06:26:02 +0100

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,8 @@
+nano /etc/apt/sources.list
+
+deb https://kali.download/kali/ kali-rolling main non-free contrib
+deb https://ftp.debian.org/debian stable main contrib non-free
+deb https://http.kali.org/kali kali-rolling main non-free contrib
+deb https://http.kali.org/kali kali-last-snapshot main non-free contrib
+deb https://http.kali.org/kali kali-experimental main non-free contrib
+deb-src https://http.kali.org/kali kali-rolling main non-free contrib
